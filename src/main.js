5543651db48f7a630b62d1b3d027bcb0276b8e82
Pei <gonsux.3686@gmail.com>
2022-08-29 00:35:25 +0800

    安裝 Font Awesome

--- src/main.js
+++ src/main.js
@@ -4,6 +4,8 @@ import router from './router'
 import 'bootstrap'
 import axios from 'axios'
 import VueAxios from 'vue-axios'
+import '@fortawesome/fontawesome-free/css/all.css'
+import '@fortawesome/fontawesome-free/js/all.js'
 
 const app = createApp(App)
 app.use(router)
--- src/views/front/IndexPage.vue
+++ src/views/front/IndexPage.vue
@@ -1,3 +1,5 @@
 <template>
   <h1>test</h1>
+  <i class="fas fa-align-left"></i>
+
 </template>
