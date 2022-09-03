838bdc5fcce55ee5f7fa3575fa8983f67aa25e24
Pei <gonsux.3686@gmail.com>
2022-09-03 23:47:55 +0800

    搜尋結果頁的 card

---
+++ src/components/SearchForm.vue
@@ -0,0 +1,77 @@
+<template>
+  <div class="container">
+    <div class="row mt-10 justify-content-center">
+      <div class="col-md-6 col-lg-4">
+        <div>
+          <input
+            type="search"
+            class="
+              form-control
+              shadow
+              mb-6
+              py-3 py-md-5
+              px-5 px-md-7
+              fs-5
+              text-primary
+              border-0
+              rounded-pill
+            "
+            placeholder="搜尋食譜"
+          />
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-4">
+        <select class="form-select  py-3 py-md-5 mb-6
+              px-5 px-md-7 rounded-pill shadow  border-0
+              fs-5" aria-label="Default select example">
+          <option selected disabled class="">選擇食材</option>
+          <option value="1">雞蛋</option>
+          <option value="2">雞肉</option>
+          <option value="3">絲瓜</option>
+        </select>
+      </div>
+      <div class="col-lg-8">
+        <div class="d-flex flex-wrap flex-lg-nowrap">
+          <select class="form-select tl-radius py-3 py-md-5
+                px-5 px-md-7 shadow  border-0 w-50 radius-lg-bl-30
+                fs-5" aria-label="Default select example">
+            <option selected disabled class="">不限時</option>
+            <option value="1">限時5分鐘</option>
+            <option value="1">限時10分鐘</option>
+            <option value="1">限時20分鐘</option>
+            <option value="1">限時30分鐘</option>
+          </select>
+          <select class="form-select  py-3 py-md-5 w-50
+                px-5 px-md-7 shadow tr-radius border-0 radius-lg-0
+                fs-5" aria-label="Default select example">
+            <option selected disabled class="">葷素皆宜</option>
+            <option value="1">蛋奶素</option>
+            <option value="1">葷</option>
+          </select>
+          <select class="form-select  py-3 py-md-5 w-50 radius-lg-0
+                px-5 px-md-7 shadow  border-0 bl-radius
+                fs-5" aria-label="Default select example">
+            <option selected disabled class="">設備</option>
+            <option value="1">平底鍋</option>
+            <option value="1">電鍋</option>
+            <option value="1">鐵鍋</option>
+          </select>
+          <select class="form-select  py-3 py-md-5 w-50
+                px-5 px-md-7 shadow border-0 br-radius radius-lg-tr-30
+                fs-5" aria-label="Default select example">
+            <option selected disabled class="">放置類型</option>
+            <option value="1">非放置類型</option>
+            <option value="1">可放置類型</option>
+          </select>
+        </div>
+      </div>
+      <div class="col-md-12">
+        <input
+            type="button"
+            value="搜尋"
+            class="mt-6 fs-5 px-7 btn btn-primary rounded-pill text-own-white"
+          />
+      </div>
+    </div>
+  </div>
+</template>
--- src/views/front/SearchResults.vue
+++ src/views/front/SearchResults.vue
@@ -1,112 +1,53 @@
 <template>
-  <div class="container">
-    <div class="row mt-10 justify-content-center">
-      <div class="col-md-6 col-lg-4">
-        <div>
-          <input
-            type="search"
-            class="
-              form-control
-              shadow
-              mb-6
-              py-3 py-md-5
-              px-5 px-md-7
-              fs-5
-              text-primary
-              border-0
-              rounded-pill
-            "
-            placeholder="搜尋食譜"
-          />
-        </div>
-      </div>
-      <div class="col-md-6 col-lg-4">
-        <select class="form-select  py-3 py-md-5 mb-6
-              px-5 px-md-7 rounded-pill shadow  border-0
-              fs-5" aria-label="Default select example">
-          <option selected class="">選擇食材</option>
-          <option value="1">雞蛋</option>
-          <option value="2">雞肉</option>
-          <option value="3">絲瓜</option>
-        </select>
-        <!-- <div class="">
-            <div class="dropdown">
-              <button
-                class="
-                  btn btn-own-white
-                  dropdown-toggle
-                  shadow
-                  text-start text-primary
-                  w-100
-                  py-3 py-md-5
-                  px-5 px-md-7
-                  fs-5
-                  rounded-pill
-                "
-                type="button"
-                id="dropdownMenuButton1"
-                data-bs-toggle="dropdown"
-                aria-expanded="false"
-              >
-                選擇食材
-              </button>
-              <ul
-                class="dropdown-menu border-0 w-75 ms-8 mb-2 fs-5"
-                aria-labelledby="dropdownMenuButton1"
-              >
-                <li><a class="dropdown-item text-primary" href="#">雞蛋</a></li>
-                <li><a class="dropdown-item text-primary" href="#">地瓜</a></li>
-                <li>
-                  <a class="dropdown-item text-primary" href="#">玉米筍</a>
-                </li>
-              </ul>
+  <searchform></searchform>
+  <div class="container mt-10">
+    <div class="row">
+      <h2 class="text-dark-green text-start">共10道食譜</h2>
+      <div class="col-md-6">
+        <a href="#" class="text-reset text-decoration-none">
+          <div class="card border-0 h-100 bg-card-green-100 p-4 py-lg-6 py-lg-7" style="">
+            <div class="row g-0 flex-nowrap align-items-center align-items-lg-start">
+              <div class="col-md-4 col-3 line-before">
+                <div class=""></div>
+                  <img
+                      class="w-100
+                        w-sm-75 w-lg-75
+                        img-fluid
+                        border-0
+                        rounded-1
+                      "
+                      src="../../libs/img/recipe/涼拌豆腐.jpg"
+                      alt="蛤蠣湯.jpg"
+                    />
+                  <button class="text-center mt-3 btn btn-primary text-own-white rounded-pill align-middle d-lg-inline-block d-none">
+                          <i class="fa-solid fa-utensils fs-6 me-3"></i>加入餐點
+                    </button>
+              </div>
+              <div class="col-md-8 col-9">
+                <div class="card-body ms-lg-10 text-start d-flex align-items-center p-0">
+                  <div class="card-body p-md-0 text-start text-dark-green ">
+                        <h6 class="card-title">西西里檸檬氣泡飲</h6>
+                        <p class="card-text m-0">5分鐘</p>
+                        <p class="card-text m-0">食材：雪碧、咖啡、檸檬</p>
+                      </div>
+                  <button class="btn text-primary d-lg-none  w-lg-15">
+                        <i class="fa-solid fa-utensils px-2 px-sm-5 px-md-2 fs-5"></i>
+                  </button>
+                </div>
+              </div>
             </div>
-          </div> -->
-      </div>
-      <div class="col-lg-8">
-        <div class="d-flex">
-          <select class="form-select  py-3 py-md-5
-                px-5 px-md-7 shadow  border-0
-                fs-5" aria-label="Default select example">
-            <option selected class="">不限時</option>
-            <option value="1">限時5分鐘</option>
-            <option value="1">限時10分鐘</option>
-            <option value="1">限時20分鐘</option>
-            <option value="1">限時30分鐘</option>
-          </select>
-          <select class="form-select  py-3 py-md-5
-                px-5 px-md-7 shadow  border-0
-                fs-5" aria-label="Default select example">
-            <option selected class="">葷素皆宜</option>
-            <option value="1">蛋奶素</option>
-            <option value="1">葷</option>
-          </select>
-        </div>
-        <div class="d-flex">
-          <select class="form-select  py-3 py-md-5
-                px-5 px-md-7 shadow  border-0
-                fs-5" aria-label="Default select example">
-            <option selected class="">設備</option>
-            <option value="1">平底鍋</option>
-            <option value="1">電鍋</option>
-            <option value="1">鐵鍋</option>
-          </select>
-          <select class="form-select  py-3 py-md-5
-                px-5 px-md-7 shadow  border-0
-                fs-5" aria-label="Default select example">
-            <option selected class="">放置類型</option>
-            <option value="1">非放置類型</option>
-            <option value="1">可放置類型</option>
-          </select>
-        </div>
-      </div>
-      <div class="col-md-12">
-        <input
-            type="button"
-            value="搜尋"
-            class="mt-6 fs-5 px-7 btn btn-primary rounded-pill text-own-white"
-          />
+          </div>
+        </a>
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import searchform from '@/components/SearchForm.vue'
+export default {
+  components: {
+    searchform
+  }
+}
+</script>
