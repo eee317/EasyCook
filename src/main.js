b075383b8086fdc361a6e8c7b34bce021190d51f
Pei <gonsux.3686@gmail.com>
2022-09-04 17:51:31 +0800

    安裝 pinia

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import App from './App.vue'
 import router from './router'
 import 'bootstrap'
@@ -7,7 +8,9 @@ import VueAxios from 'vue-axios'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
 
+const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
+app.use(pinia)
 app.mount('#app')
