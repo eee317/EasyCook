7a8c2b7db777b4749eafaa81f2f576c0d549928c
Pei <gonsux.3686@gmail.com>
2022-09-07 17:04:30 +0800

    移除 swiper 全域設定

--- src/main.js
+++ src/main.js
@@ -7,15 +7,10 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
-// import { Swiper, SwiperSlide } from 'swiper/vue'
 
-// const swiper = Swiper()
-// const swiperSlide = SwiperSlide()
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
 app.use(pinia)
-// app.use(swiper)
-// app.use(swiperSlide)
 app.mount('#app')
