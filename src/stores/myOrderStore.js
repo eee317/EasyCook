7d6f814d5bf296f45deb63e2291b3fa057087417
Pei <gonsux.3686@gmail.com>
2022-09-12 23:10:52 +0800

    myorder 加入for迴圈做時間運算，切版冰箱

--- src/stores/myOrderStore.js
+++ src/stores/myOrderStore.js
@@ -2,7 +2,10 @@ import axios from 'axios'
 import { defineStore } from 'pinia'
 export default defineStore('myOrderStore', {
   state: () => ({
-    orders: []
+    orders: [],
+    timeTotal: 0,
+    overlappingTimeTotal: 0,
+    cookTimeTotal: 0
   }),
   actions: {
     getMyOrder () {
@@ -11,7 +14,23 @@ export default defineStore('myOrderStore', {
           this.orders = res.data
           console.log(this.orders)
           console.log(this.orders.length)
+          this.runTimeTotal()
+          this.runOverlappingTimeTotal()
+          this.runCookTimeTotal()
         })
+    },
+    runTimeTotal () {
+      for (let i = 0; i < this.orders.length; i++) {
+        this.timeTotal += this.orders[i].time
+      }
+    },
+    runOverlappingTimeTotal () {
+      for (let i = 0; i < this.orders.length; i++) {
+        this.overlappingTimeTotal += this.orders[i].overlappingTime
+      }
+    },
+    runCookTimeTotal () {
+      this.cookTimeTotal = this.timeTotal - this.overlappingTimeTotal
     }
   }
 })
--- src/views/front/MyOrder.vue
+++ src/views/front/MyOrder.vue
@@ -2,7 +2,7 @@
   <div class="container">
     <h1 class="text-dark-green mt-15">我的餐車</h1>
   </div>
-  <div class="container mt-15">
+  <div class="container mt-lg-15">
     <div class="row justify-content-md-between flex-md-row-reverse">
       <div class="col-lg-9 col-md-8">
         <div class="d-flex d-lg-inline my-5 float-md-end">
@@ -60,7 +60,7 @@
               <td class="border-0 pt-6"></td>
               <td class="border-0 pt-6"></td>
               <td class="border-0 pt-6 fs-lg-6">烹飪時間 / 分</td>
-              <td class="border-0 pt-6 fs-lg-6">20 分</td>
+              <td class="border-0 pt-6 fs-lg-6">{{timeTotal}} 分</td>
             </tr>
             <tr class="text-dark-green">
               <td class="border-0"></td>
@@ -68,55 +68,86 @@
               <td class="border-0"></td>
               <td class="border-0"></td>
               <td class="border-bottom border-3 border-card-img-green-200 fs-lg-6">放置時間 / 分</td>
-              <td class="border-bottom border-3 border-card-img-green-200 fs-lg-6">0 分</td>
+              <td class="border-bottom border-3 border-card-img-green-200 fs-lg-6">{{overlappingTimeTotal}} 分</td>
             </tr>
             <tr class="text-dark-green fw-bold">
               <td class="border-0"></td>
               <td class="border-0 d-none d-lg-table-cell"></td>
               <td class="border-0"></td>
               <td class="border-0"></td>
-              <th class="py-4 border-0 fs-lg-6">放置時間 / 分</th>
-              <td class="py-4 border-0 fs-lg-6">20 分</td>
+              <th class="py-4 border-0 fs-lg-6">總烹飪時間 / 分</th>
+              <td class="py-4 border-0 fs-lg-6">{{cookTimeTotal}} 分</td>
             </tr>
           </tfoot>
         </table>
       </div>
     </div>
   </div>
+  <div class="myOrder-equipment-bg py-10 mt-15">
+    <div class="container">
+      <div class="row">
+        <h2 class="h1 text-dark-green text-lg-start">所需的廚房用品</h2>
+        <div class="">
+          <table class="w-100 my-5 table ">
+            <thead class="fs-md-5 text-card-green-300 border-2">
+              <tr class="border-bottom border-card-green-300">
+                <th scope="col"></th>
+                <th scope="col" class="d-none d-lg-table-cell" width="180"></th>
+                <th scope="col">餐點</th>
+                <th scope="col">廚具用品</th>
+                <th scope="col">醬料</th>
+              </tr>
+            </thead>
+            <tbody class="text-dark-green">
+              <tr class="border-bottom border-card-green-300 border-1"
+              v-for="order in orders" :key="order.id+'order'">
+                <th scope="row" class="py-4  rounded-start align-middle">
+                  <i class="text-card-green-300 h3
+                  fa-solid fa-circle-xmark m-0"></i>
+                </th>
+                <td class="py-4 d-none d-lg-table-cell  border-card-green-300"><img width="180" class="img-fluid rounded-2 border border-card-green-300 border-2 rounded-2"
+                :src="`${order.image}`" :alt="`${order.title}.jpg`" ></td>
+                <td class="py-4 border-card-green-300 align-middle fs-lg-6">{{order.title}}</td>
+                <td class="py-4 border-top border-card-green-300 align-middle fs-lg-6">
+                  <p class="m-0" v-if="order.equipment.length===0"> - </p>
+                  <p v-for="equipment in order.equipment"   :key="equipment" class="m-0 lh-base">
+                    {{equipment}}
+                  </p></td>
+                <td class="py-4 border-card-green-300 align-middle fs-lg-6">
+                  <p class="m-0" v-if="order.sauce.length===0"> - </p>
+                  <p v-for="sauce in order.sauce"   :key="sauce" class="m-0 lh-base">
+                    {{sauce}}
+                  </p></td>
+              </tr>
+            </tbody>
+          </table>
+        </div>
+      </div>
+    </div>
+  </div>
   <div class="container">
     <div class="row">
-      <h2 class="h1 text-dark-green mt-15 text-lg-start">所需的廚房用品</h2>
-      <div class="myOrder-equipment">
-        <table class="w-100 my-5 table ">
-          <thead class="fs-md-5 text-card-green-300">
-            <tr>
-              <th scope="col"></th>
-              <th scope="col" class="d-none d-lg-table-cell" width="180"></th>
-              <th scope="col">餐點</th>
-              <th scope="col">廚具用品</th>
-              <th scope="col">醬料</th>
-            </tr>
-          </thead>
-          <tbody class="text-dark-green">
-            <tr v-for="order in orders" :key="order.id+'order'">
-              <th scope="row" class="py-4  rounded-start align-middle">
-                <i class="text-card-green-300 h3
-                fa-solid fa-circle-xmark m-0"></i>
-              </th>
-              <td class="py-4 d-none d-lg-table-cell "><img width="180" class="img-fluid rounded-2 border border-card-green-300 border-2 rounded-2"
-              :src="`${order.image}`" alt="" ></td>
-              <td class="py-4 align-middle fs-lg-6">{{order.title}}</td>
-              <td class="py-4 align-middle fs-lg-6">
-                <p v-for="equipment in order.equipment"   :key="equipment" class="m-0 lh-base">
-                  {{equipment}}
-                </p></td>
-              <td class="py-4 align-middle fs-lg-6">
-                <p v-for="sauce in order.sauce"   :key="sauce" class="m-0 lh-base">
-                  {{sauce}}
-                </p></td>
-            </tr>
-          </tbody>
-        </table>
+      <div class="text-start">
+        <h2 class="text-dark-green mt-15 mb-5">我的冰箱還有食材</h2>
+        <p class="text-dark-green m-0 pb-5">發現前幾天煮完飯還有剩餘的食材，請先設定冰箱有哪些食材，幫您計算還需要購買多少食材。</p>
+      </div>
+      <div>
+        <div class="card border-0 bg-bright-green pb-5">
+          <div class="card-body p-0">
+            <div class="border-bottom border-1 d-flex justify-content-between align-items-center py-3 px-5">
+              <h5 class="card-title text-own-brown text-start m-0">我的冰箱</h5>
+              <button type="button" class="btn btn-primary text-own-white rounded-pill border-0"><i class="me-3 fa-solid fa-circle-plus align-middle"></i>新增</button>
+            </div>
+            <div class="d-flex justify-content-between align-items-center py-3 px-5 text-own-brown">
+              <h6 class="color-own-brown-lighten">食材名稱</h6>
+              <h6 class="color-own-brown-lighten">數量</h6>
+            </div>
+            <div class="text-own-brown px-5 d-flex">
+              <input class="form-control tl-bl-radius" type="text" placeholder="Default input" aria-label="default input example">
+              <input class="form-control tr-br-radius tl-bl-radius text-end" type="text" placeholder="Default input" aria-label="default input example">
+            </div>
+          </div>
+        </div>
       </div>
     </div>
   </div>
@@ -125,8 +156,12 @@
 import { mapState, mapActions } from 'pinia'
 import myOrderStore from '@/stores/myOrderStore'
 export default {
+  data () {
+    return {
+    }
+  },
   computed: {
-    ...mapState(myOrderStore, ['orders'])
+    ...mapState(myOrderStore, ['orders', 'timeTotal', 'overlappingTimeTotal', 'cookTimeTotal'])
   },
   methods: {
     ...mapActions(myOrderStore, ['getMyOrder'])
