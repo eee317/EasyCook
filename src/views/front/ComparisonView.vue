ff33b44d007d3c43da448659214f70edd415135d
Pei <gonsux.3686@gmail.com>
2022-09-13 17:04:25 +0800

    新增比對頁面

---
+++ src/views/front/ComparisonView.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="container">
+    <div class="row justify-content-center">
+      <h1 class="text-dark-green mt-15">需購買的食材清單</h1>
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-8">
+        <div class="d-flex justify-content-md-between my-5">
+          <router-link to="/myOrder" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-outline-own-orange tl-bl-radius radius-md-all  py-3 px-5 px-md-7 me-md-4 fs-md-5"><i class="me-2 align-middle h3
+                fa-solid fa-circle-arrow-left"></i>返回餐點</router-link>
+          <button type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-own-orange tr-br-radius radius-md-all text-own-white py-3 px-5 fs-md-5"><i class="me-2 align-middle h3 fa-solid fa-pen-to-square"></i>編輯冰箱</button>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
