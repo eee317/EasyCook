9254c2dedace9b95b150f90ca8ee4ce0cc7edfb4
Pei <gonsux.3686@gmail.com>
2022-09-13 17:03:03 +0800

    完成MyOrder頁面

--- src/views/front/MyOrder.vue
+++ src/views/front/MyOrder.vue
@@ -6,14 +6,14 @@
     <div class="row justify-content-md-between flex-md-row-reverse">
       <div class="col-lg-9 col-md-8">
         <div class="d-flex d-lg-inline my-5 float-md-end">
-          <button type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-outline-primary tl-bl-radius radius-md-all  py-3 py-md-5 px-5 px-md-7 me-md-4 fs-5"><i class="me-2 align-middle h3
+          <button type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-outline-primary tl-bl-radius radius-md-all  py-3 px-5 px-md-7 me-md-4 fs-md-5"><i class="me-2 align-middle h3
                 fa-solid fa-circle-xmark"></i>清除所有餐點</button>
-          <button type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-primary tr-br-radius radius-md-all text-own-white py-3 py-md-5 px-5 fs-5"><i class="me-2 align-middle h3 fa-solid fa-book-open"></i>逛逛食譜</button>
+          <button type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-primary tr-br-radius radius-md-all text-own-white py-3 px-5 fs-md-5"><i class="me-2 align-middle h3 fa-solid fa-book-open"></i>逛逛食譜</button>
         </div>
       </div>
       <div class="col-lg-3 col-md-4">
-        <select class="form-select py-3 py-md-5 mb-6 mb-md-0 my-md-5 px-5 px-md-7 rounded-pill shadow  border-0
-                fs-5" aria-label="Default select example">
+        <select class="form-select py-3 mb-6 mb-md-0 my-md-5 px-5 px-md-7 rounded-pill shadow  border-0
+                fs-md-5" aria-label="Default select example">
           <option selected class="">1 / 人</option>
           <option value="2">2 / 人</option>
           <option value="3">3 / 人</option>
@@ -83,10 +83,10 @@
       </div>
     </div>
   </div>
-  <div class="myOrder-equipment-bg py-10 mt-15">
+  <div class="myOrder-equipment-bg py-10 mt-7 mt-md-15">
     <div class="container">
       <div class="row">
-        <h2 class="h1 text-dark-green text-lg-start">所需的廚房用品</h2>
+        <h2 class=" text-dark-green text-lg-start">所需的廚房用品</h2>
         <div class="">
           <table class="w-100 my-5 table ">
             <thead class="fs-md-5 text-card-green-300 border-2">
@@ -105,7 +105,7 @@
                   <i class="text-card-green-300 h3
                   fa-solid fa-circle-xmark m-0"></i>
                 </th>
-                <td class="py-4 d-none d-lg-table-cell  border-card-green-300"><img width="180" class="img-fluid rounded-2 border border-card-green-300 border-2 rounded-2"
+                <td class="py-4 d-none d-lg-table-cell  border-card-green-300"><img width="100" class="img-fluid rounded-2 border border-card-green-300 border-2 rounded-2"
                 :src="`${order.image}`" :alt="`${order.title}.jpg`" ></td>
                 <td class="py-4 border-card-green-300 align-middle fs-lg-6">{{order.title}}</td>
                 <td class="py-4 border-top border-card-green-300 align-middle fs-lg-6">
@@ -126,28 +126,38 @@
     </div>
   </div>
   <div class="container">
-    <div class="row">
-      <div class="text-start">
-        <h2 class="text-dark-green mt-15 mb-5">我的冰箱還有食材</h2>
-        <p class="text-dark-green m-0 pb-5">發現前幾天煮完飯還有剩餘的食材，請先設定冰箱有哪些食材，幫您計算還需要購買多少食材。</p>
+    <div class="row justify-content-center">
+      <div class="col-md-7 col-lg-8 text-start mb-lg-4">
+        <h2 class="text-md-center text-dark-green mt-15 mt-lg-20 mb-5">我的冰箱還有食材</h2>
+        <p class="text-dark-green m-0 pb-5 fs-lg-6">發現前幾天煮完飯還有剩餘的食材，請先設定冰箱有哪些食材，幫您計算還需要購買多少食材。</p>
       </div>
-      <div>
-        <div class="card border-0 bg-bright-green pb-5">
+    </div>
+    <div class="row justify-content-center">
+      <div class="col-md-4 col-lg-5 mb-5">
+        <div class="card border-0 bg-bright-green pb-2 h-100">
           <div class="card-body p-0">
-            <div class="border-bottom border-1 d-flex justify-content-between align-items-center py-3 px-5">
-              <h5 class="card-title text-own-brown text-start m-0">我的冰箱</h5>
+            <div class="border-bottom border-1 d-flex justify-content-between align-items-center py-3 py-lg-5 px-5">
+              <h5 class="card-title text-own-brown text-start m-0 fs-lg-5">我的冰箱</h5>
               <button type="button" class="btn btn-primary text-own-white rounded-pill border-0"><i class="me-3 fa-solid fa-circle-plus align-middle"></i>新增</button>
             </div>
             <div class="d-flex justify-content-between align-items-center py-3 px-5 text-own-brown">
-              <h6 class="color-own-brown-lighten">食材名稱</h6>
-              <h6 class="color-own-brown-lighten">數量</h6>
+              <h6 class="color-own-brown-lighten ms-4 fs-lg-6">食材名稱</h6>
+              <h6 class="color-own-brown-lighten me-4 fs-lg-6">數量</h6>
             </div>
-            <div class="text-own-brown px-5 d-flex">
-              <input class="form-control tl-bl-radius" type="text" placeholder="Default input" aria-label="default input example">
-              <input class="form-control tr-br-radius tl-bl-radius text-end" type="text" placeholder="Default input" aria-label="default input example">
+            <div class="text-own-brown px-5 d-flex mb-3">
+              <input class="px-4 py-3 form-control tl-bl-radius border-0 border-top border-bottom border-start border-own-brown text-own-brown fs-lg-6" type="text" placeholder="請輸入文字" aria-label="default input example" value="百香果">
+              <input class="pe-5 py-3 form-control tr-br-radius tl-bl-radius text-end border-0 border-top border-end border-bottom border-start border-own-brown text-own-brown fs-lg-6" type="number" placeholder="請輸入數字" aria-label="default input example" value="1">
             </div>
           </div>
         </div>
+      </div>
+      <div class="mb-10 mt-5 col-md-4 ">
+        <h2 class="h1 fs-md-3 fw-bold text-own-orange">冰箱設定完成</h2>
+        <div class="myOrder-finishBg py-4">
+          <img class="img-fluid w-40"
+          src="../../../src/libs/img/illustrations/undraw_join_re_w1lh.svg" alt="前往下一步.svg">
+        </div>
+        <router-link to="/comparison" class="btn btn-own-orange text-own-white py-3 py-md-2 px-7 px-md-7 fs-6 rounded-pill"><i class="fa-brands fa-golang align-middle me-3 fs-3"></i>前往比對</router-link>
       </div>
     </div>
   </div>
