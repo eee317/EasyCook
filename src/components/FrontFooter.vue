97c013f3b53bcf4553daca24af543ecee868f986
Pei <gonsux.3686@gmail.com>
2022-09-13 23:36:49 +0800

    調整footer

--- src/components/FrontFooter.vue
+++ src/components/FrontFooter.vue
@@ -1,6 +1,6 @@
 <template>
-  <footer class="bg-dark-green mt-10 mt-md-15 pt-5 pb-4">
-    <p class="m-0 text-card-img-green-200">本網站僅供個人作品使用，不提供商業用途</p>
+  <footer class="bg-dark-green mt-10 mt-md-15 pt-5 pb-4 px-3">
+    <p class="m-0 text-card-img-green-200 mb-3 mb-sm-0">本網站僅供個人作品使用，不提供商業用途</p>
     <a href="mailto:eee317@gmail.com" class="text-card-img-green-200 fs-5"><i class="fa-regular fa-envelope"></i></a>
     <a href="https://github.com/eee317/EasyCook" class="text-card-img-green-200 ms-3 fs-5"><i class="fa-brands fa-github"></i></a>
   </footer>
