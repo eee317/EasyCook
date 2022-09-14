63944058cb0f8164439d54b2b35936cafd4a1489
Pei <gonsux.3686@gmail.com>
2022-09-14 22:51:14 +0800

    安裝 masonry

--- src/main.js
+++ src/main.js
@@ -7,10 +7,12 @@ import axios from 'axios'
 import VueAxios from 'vue-axios'
 import '@fortawesome/fontawesome-free/css/all.css'
 import '@fortawesome/fontawesome-free/js/all.js'
+import { VueMasonryPlugin } from 'vue-masonry'
 
 const pinia = createPinia()
 const app = createApp(App)
 app.use(router)
 app.use(VueAxios, axios)
 app.use(pinia)
+app.use(VueMasonryPlugin)
 app.mount('#app')
