2eb64df7afb0330d4553b1c1ad218d8d11c77429
Pei <gonsux.3686@gmail.com>
2022-09-15 22:58:12 +0800

    修正 btn hover 文字顏色

--- src/views/front/ComparisonView.vue
+++ src/views/front/ComparisonView.vue
@@ -7,7 +7,7 @@
       <div class="row justify-content-center">
         <div class="col-md-8 col-lg-6">
           <div class="d-flex justify-content-md-between my-5">
-            <router-link to="/myOrder" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-outline-own-orange tl-bl-radius radius-md-all  py-3 px-5 px-md-7 me-md-4 fs-md-5"><i class="me-2 align-middle h3
+            <router-link to="/myOrder" class="btn-hover-text-white flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-outline-own-orange tl-bl-radius radius-md-all  py-3 px-5 px-md-7 me-md-4 fs-md-5"><i class="me-2 align-middle h3
                   fa-solid fa-circle-arrow-left"></i>返回餐點</router-link>
             <button @click="openModal" type="button" class="flex-fill flex-lg-grow-0 flex-lg-shrink-0 btn btn-own-orange tr-br-radius radius-md-all text-own-white py-3 px-5 fs-md-5"><i class="me-2 align-middle h3 fa-solid fa-pen-to-square"></i>編輯冰箱</button>
           </div>
