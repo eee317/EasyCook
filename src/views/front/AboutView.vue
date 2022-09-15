184b97f66cbe994c54d08fa5414742658807fe43
Pei <gonsux.3686@gmail.com>
2022-09-15 22:45:47 +0800

    更新文字資訊

--- src/views/front/AboutView.vue
+++ src/views/front/AboutView.vue
@@ -8,16 +8,18 @@
       <div class="row text-dark-green justify-content-center">
         <div class="mt-4 col-md-7 col-10">
           <h2 class="py-4 text-md-start">一起輕鬆料理吧！</h2>
-          <p class="text-start lh--lg">受夠外面油膩的餐點，想要吃一頓健康的一餐，但是又沒有太多時間下廚，光是備料就容易無法精準拿捏人數，而煩惱不已嗎？</p>
+          <p class="text-start lh-lg">受夠外面油膩的餐點，想要吃一頓健康的一餐，但是又沒有太多時間下廚，光是備料就容易無法精準拿捏人數，而煩惱不已嗎？</p>
           <p class="text-start lh-lg">這裡為您提供一個簡易方便料理的食譜，可以透過食材做搜尋，加入餐車後，為您計算下廚所需花費的時間共有多少，有效率的煮完這一餐。</p>
         </div>
         <div class="mt-4 col-md-7 col-10">
           <h2 class="py-4 text-md-start">網站起源</h2>
-          <p class="text-start lh-lg">一群熱愛 UX 的四位夥伴:Ｗindy, Lara, 蘇菲、田田，一起參加了 IDEO 舉辦的 UX 活動，一起選了關於「吃」的題目，歷經兩個月的時間，我們一起做了訪談、問卷調查、便利貼分類法、用戶旅程圖，最後發現現代人想追求健康，卻不容易自己下廚。為了能有效率地利用時間煮飯，快速得知自己所需的食材、完善的在時間內處理料理，因此誕生這個網站。</p>
+          <p class="text-start lh-lg">與一群熱愛 UX 的夥伴們，一起參加了 IDEO 舉辦的 UX 活動，一起選了關於「吃」的題目，歷經兩個月的時間，我們一起做了訪談、問卷調查、便利貼分類法、用戶旅程圖，最後發現現代人想追求健康，卻不容易自己下廚。為了能有效率地利用時間煮飯，快速得知自己所需的食材、完善的在時間內處理料理，因此誕生這個網站。</p>
+          <p class="text-start lh-lg">特別感謝 UX 研究夥伴：Windy, Lara, 蘇菲</p>
         </div>
         <div class="mt-4 col-md-7 col-10">
           <h2 class="py-4 text-md-start">版權宣告</h2>
           <p class="text-start lh-lg">輕鬆煮廚網站 圖片 及 文字 僅供個人作品使用，不提供轉載、引用及商業用途。</p>
+          <p class="text-start lh-lg m-0">icon 來源：<a class=" text-decoration-none" href="https://fontawesome.com/icons/pen-to-square?s=solid&f=classic">Font Awesome</a></p>
           <p class="text-start lh-lg m-0">圖片來源：<a class=" text-decoration-none" href="https://undraw.co/illustrations?fbclid=IwAR2Ta9IMIFpHAJC46VTFBGwKJDtpF10NMicNr07Z5B7MdMcTkmxXjWsZlsY">unDraw</a></p>
           <p class="text-start lh-lg">食譜插圖：輕鬆煮廚</p>
         </div>
